e033f294955898a91e29bd4ec0e062fd29d65221
Vinodhini Devaraj <145280558+vinodhinidevaraj@users.noreply.github.com>
2026-02-17 19:42:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const cards = document.querySelectorAll('.card');
 window.addEventListener('scroll', () => {
     cards.forEach(card => {
         const cardTop = card.getBoundingClientRect().top;
-        if(cardTop < window.innerHeight - 100) {
+        if (cardTop < window.innerHeight - 100) {
             card.classList.add('show');
         }
     });
